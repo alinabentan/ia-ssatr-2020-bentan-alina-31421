25c52fe70a11ab7159427168f69a8f5b8a02ecaa
unknown <alina.bentan@gmail.com>
2020-10-12 21:12:57 +0300

    Laborator1

---
+++ lab1/src/main/java/com/mycompany/lab1/Ex1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author Alina
+ */
+public class Ex1 {
+      public static void main(String[] args){
+        System.out.println("Message");
+    }
+    
+}
